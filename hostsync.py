32f8ef48b85a2ebc827dc7ecd75f146e4653fd83
lyncir <lyncir@gmail.com>
2016-11-13 12:49:19 +0000

    test ok

--- hostsync.py
+++ hostsync.py
@@ -2,9 +2,11 @@
 """
 Todo: sync https://raw.githubusercontent.com/racaljk/hosts/master/hosts to
 /etc/hosts
+use Etag and Content-Range
 """
 
 import os
+import shutil
 try:
     from urllib.request import urlopen
 except ImportError:
@@ -30,7 +32,25 @@ def download_file(url, output_file, verbose=0):
     """
     Urlclass = URLOpener()
 
-    if os.path.exists(output_file):
+    page = Urlclass.open(url)
+    if verbose:
+        for k, v in page.headers.items():
+            print('{}={}'.format(k, v))
+
+    # Etag
+    new_source = False
+    etag = page.headers.get('Etag')
+    if etag:
+        etag_file = output_file + '.etag'
+        if os.path.exists(etag_file):
+            if etag != open(etag_file).read():
+                new_source = True
+        else:
+            f = open(etag_file, 'w')
+            f.write(etag)
+            f.close()
+
+    if os.path.exists(output_file) and not new_source:
         opfile = open(output_file, 'ab')
         exist_size = os.path.getsize(output_file)
         Urlclass.addheader('Range', 'bytes={}-'.format(exist_size))
@@ -38,15 +58,15 @@ def download_file(url, output_file, verbose=0):
         opfile = open(output_file, 'wb')
         exist_size = 0
 
-    page = Urlclass.open(url)
-    #if verbose:
-    #    for k, v in page.headers.items():
-    #        print('{}={}'.format(k, v))
     num_bytes = 0
     web_size = int(page.headers['Content-Length'])
+    web_range = page.headers.get('Content-Range')
     if web_size == exist_size:
         if verbose:
             print('File ({}) was already downloaded from URL ({})'.format(output_file, url))
+        if etag:
+            if os.path.exists(etag_file):
+                os.remove(etag_file)
     else:
         if verbose:
             print('Downloading {} more bytes'.format(web_size - exist_size))
@@ -56,6 +76,9 @@ def download_file(url, output_file, verbose=0):
                 break
             opfile.write(data)
             num_bytes = num_bytes + len(data)
+        if etag:
+            if os.path.exists(etag_file):
+                os.remove(etag_file)
     page.close()
     opfile.close()
     if verbose:
@@ -65,9 +88,12 @@ def download_file(url, output_file, verbose=0):
 
 
 def main():
-    #url = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
-    url = 'http://mirrors.163.com/gentoo/releases/amd64/autobuilds/20161108/systemd/stage3-amd64-systemd-20161108.tar.bz2'
-    print(download_file(url, 'hosts.dl', 1))
+    url = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
+    src_file = '/tmp/hosts.dl'
+    dst_file = '/etc/hosts'
+    download_file(url, src_file, 1)
+    shutil.copyfile(src_file, dst_file)
+    
 
 
 if __name__ == '__main__':
